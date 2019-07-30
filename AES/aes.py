351bd60ad0018cc0214c8e060ac2d9397c5fa04b
aozturkk <47629982+aozturkk@users.noreply.github.com>
2019-07-30 14:23:09 +0300

    Update: aes.py
    Fixed Turkish character problem.

--- AES/aes.py
+++ AES/aes.py
@@ -7,10 +7,11 @@ import hashlib
 
 def encrypt(plain_text,key,iv):
 
+	plain_text = base64.b64encode(plain_text.encode()).decode()
 	backend = default_backend()
 	cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
 	encryptor = cipher.encryptor()
-	plain_text_byte = pad(plain_text).encode('utf-8')
+	plain_text_byte = pad(plain_text).encode()
 	buf = bytearray( 2*len(plain_text_byte) - 1  )
 	len_encrypted = encryptor.update_into(plain_text_byte, buf)
 	ct = bytes(buf[:len_encrypted]) + encryptor.finalize()	
@@ -24,13 +25,15 @@ def decrypt(encrypt_text,key,iv):
 	base64_decoded = base64.b64decode(encrypt_text)
 	buf = bytearray( 2*len(base64_decoded) - 1  )
 	len_decrypted = decryptor.update_into(base64_decoded, buf)
-	return bytes(buf[:len_decrypted]) + decryptor.finalize()
+	dec = bytes(buf[:len_decrypted]) + decryptor.finalize()
+	dec = unpad(dec).decode()
+	return base64.b64decode(dec).decode()
 	
 def pad(s):
     return s + (32 - len(s) % 32) * chr(32 - len(s) % 32)	
 
 def unpad(s):
-        return s[:-ord(s[len(s)-1:])]	
+	return s[:-ord(s[len(s)-1:])]	
 
 print("1- Encrypt \n2- Decrypt \nSelect Option :")	
 option = input()	
@@ -44,8 +47,8 @@ key = hashlib.md5( hashlib.sha256( (key).encode() ).hexdigest().encode() ).hexdi
 iv = '4377777172699a75'.encode()
 
 if option == '1':
-	enc = encrypt(text,key,iv).decode('utf-8')
+	enc = encrypt(text,key,iv).decode()
 	print("Encrypt :",enc)
 else :	
-	dec = unpad(decrypt(text,key,iv)).decode('utf-8')	
+	dec = decrypt(text,key,iv)	
 	print("Decrypt :",dec)
